9199906b4e32b98933195d19da86ccfad4331af9
douglashenr <douglash98@hotmail.com>
2021-10-02 22:53:06 -0300

    Primeira versao do app vendo sobre InputTextLayout

---
+++ app/src/main/java/com/dhsoftware/treinamentodelayout/ExemploTelaCadastro.java
@@ -0,0 +1,32 @@
+package com.dhsoftware.treinamentodelayout;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class ExemploTelaCadastro extends Fragment {
+
+
+
+    public ExemploTelaCadastro() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_exemplo_tela_cadastro, container, false);
+    }
+}
