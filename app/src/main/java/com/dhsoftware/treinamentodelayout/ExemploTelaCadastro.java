edbe8478cc3f02f812c5a9dfa7c93b8795b9885a
douglashenr <douglash98@hotmail.com>
2021-10-03 21:32:10 -0300

    Adicionando prefixo para valor, dropdown para os estados, limitador de caracteres com contador

--- app/src/main/java/com/dhsoftware/treinamentodelayout/ExemploTelaCadastro.java
+++ app/src/main/java/com/dhsoftware/treinamentodelayout/ExemploTelaCadastro.java
@@ -7,11 +7,18 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 
 public class ExemploTelaCadastro extends Fragment {
 
-
+    private View view;
+    AutoCompleteTextView autoCompleteTextViewEstados;
 
     public ExemploTelaCadastro() {
         // Required empty public constructor
@@ -27,6 +34,15 @@ public class ExemploTelaCadastro extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_exemplo_tela_cadastro, container, false);
+        return view = inflater.inflate(R.layout.fragment_exemplo_tela_cadastro, container, false);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        ArrayAdapter<String> adapter = new ArrayAdapter<>
+                (getActivity().getApplicationContext(), android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.estados));
+        autoCompleteTextViewEstados = view.findViewById(R.id.auto_complete_estados);
+        autoCompleteTextViewEstados.setAdapter(adapter);
     }
 }
